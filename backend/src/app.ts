9ea796ef3fcc859f6f86ab7f71a01454104dd48a
Rugwed <rugwedrjt21@gmail.com>
2026-02-17 15:04:53 +0530

    feat: Integrate clerk authentication and set up user routes.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,10 +3,14 @@ import authRoute from './routes/authRoute';
 import userRoute from './routes/userRoute';
 import messageRoute from './routes/messageRoute';
 import chatRoute from './routes/chatRoute';
+import { clerkMiddleware } from '@clerk/express'
+import { errorHandler } from './middleware/errorHandler';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(clerkMiddleware())
 
 app.get("/helgth", (req, res) => {
     res.json({ status: "ok", message: "server is running" })
@@ -17,4 +21,8 @@ app.use("/api/chats", chatRoute)
 app.use("/api/messages", messageRoute)
 app.use("/api/users", userRoute)
 
+//error handlers must come after all the routes and other middlewares so they can catch errors passed with next(err)
+//  or thrown inside async handlers.
+
+app.use(errorHandler);
 export default app;
---
+++ web/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/clerk-react';
+
+function App() {
+
+  return (
+    <>
+    <h1>Helllo world</h1>
+      <SignedOut>
+        <SignInButton mode="modal" />
+      </SignedOut>
+      {/* Show the user button when the user is signed in */}
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </>
+  )
+}
+
+export default App
